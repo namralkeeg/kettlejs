adff8a17468a66716f86ec59d791417d895f7496
Larry Lopez <154121+namralkeeg@users.noreply.github.com>
2020-09-26 13:35:58 -0700

    Added generic Dictionary collection.

---
+++ src/collections/Dictionary.ts
@@ -0,0 +1,178 @@
+import { IEqualityComparer, IToString } from "./interfaces";
+import {
+  defaultEqualityComparer,
+  hashCode,
+  isDefined,
+  objectHasProperty
+} from "../utils/helpers";
+
+interface IKeyValuePair<K extends IToString, V> {
+  key: K;
+  value: V;
+}
+
+class Dictionary<K extends IToString, V> {
+  private internalValueComparer: IEqualityComparer<V>;
+  private size: number;
+  private table: { [key: number]: IKeyValuePair<K, V> };
+
+  constructor(valueComparer: IEqualityComparer<V> = defaultEqualityComparer) {
+    this.internalValueComparer = valueComparer ?? defaultEqualityComparer;
+    this.size = 0;
+    this.table = {};
+  }
+
+  public get comparer(): IEqualityComparer<V> {
+    return this.internalValueComparer;
+  }
+
+  public set comparer(comparer: IEqualityComparer<V>) {
+    this.internalValueComparer = comparer ?? defaultEqualityComparer;
+  }
+
+  public get count(): number {
+    return this.size;
+  }
+
+  public get isEmpty(): boolean {
+    return this.size <= 0;
+  }
+
+  public get keys(): Array<K> {
+    const keys = new Array<K>(this.size);
+    let i = 0;
+    for (const { key } of Object.values<IKeyValuePair<K, V>>(this.table)) {
+      keys[i++] = key;
+    }
+    return keys;
+    // return Object.values<IKeyValuePair<K, V>>(this.table).map(({ key }) => key);
+  }
+
+  public get values(): Array<V> {
+    const values = new Array<V>(this.size);
+    let i = 0;
+    for (const { value } of Object.values<IKeyValuePair<K, V>>(this.table)) {
+      values[i++] = value;
+    }
+    return values;
+    // return Object.values<IKeyValuePair<K, V>>(this.table).map(({ value }) => value);
+  }
+
+  [Symbol.iterator](): Iterator<IKeyValuePair<K, V>> {
+    const size = this.size;
+    const keys = this.keys;
+    const table = this.table;
+    let index = 0;
+
+    const iterator = {
+      next(): IteratorResult<IKeyValuePair<K, V>> {
+        let item: IteratorResult<IKeyValuePair<K, V>>;
+
+        if (index == size) {
+          item = { value: undefined, done: true };
+        } else {
+          const hash = hashCode(keys[index++].toString());
+          item = { value: table[hash], done: false };
+        }
+
+        return item;
+      }
+    };
+
+    return iterator;
+  }
+
+  public add(key: K, value: V): boolean {
+    if (!isDefined(key) || !isDefined(value)) {
+      return false;
+    }
+
+    const hash = hashCode(key.toString());
+    let status = false;
+    if (!objectHasProperty(this.table, hash)) {
+      this.table[hash] = { key, value };
+      status = true;
+      this.size++;
+    }
+
+    return status;
+  }
+
+  public clear(): void {
+    this.table = {};
+    this.size = 0;
+  }
+
+  public containsKey(key: K): boolean {
+    if (!isDefined(key)) {
+      return false;
+    }
+
+    const hash = hashCode(key.toString());
+    return objectHasProperty(this.table, hash);
+  }
+
+  public containsValue(value: V): boolean {
+    for (const { value: dictValue } of Object.values<IKeyValuePair<K, V>>(this.table)) {
+      if (this.internalValueComparer(value, dictValue)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public forEach(callback: (key: K, value: V) => unknown): void {
+    for (const { key, value } of Object.values<IKeyValuePair<K, V>>(this.table)) {
+      callback(key, value);
+    }
+  }
+
+  public get(key: K): V | undefined {
+    if (!isDefined(key)) {
+      return undefined;
+    }
+
+    const hash = hashCode(key.toString());
+    const value: V | undefined = this.table[hash]?.value;
+
+    return value;
+  }
+
+  public remove(key: K): V | undefined {
+    if (!isDefined(key)) {
+      return undefined;
+    }
+
+    const hash = hashCode(key.toString());
+    const value: V | undefined = this.table[hash]?.value;
+    if (isDefined(value)) {
+      delete this.table[hash];
+      this.size--;
+    }
+
+    return value;
+  }
+
+  public set(key: K, value: V): V | undefined {
+    if (!isDefined(key) || !isDefined(value)) {
+      return undefined;
+    }
+
+    const hash = hashCode(key.toString());
+    const item: V | undefined = this.table[hash]?.value;
+    if (!isDefined(item)) {
+      this.table[hash] = { key, value };
+      this.size++;
+    }
+
+    return item;
+  }
+
+  public toString(): string {
+    return JSON.stringify(this.table);
+  }
+}
+
+export { Dictionary };
+export default Dictionary;
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,9 +1,124 @@
+const primes = [
+  3,
+  7,
+  11,
+  17,
+  23,
+  29,
+  37,
+  47,
+  59,
+  71,
+  89,
+  107,
+  131,
+  163,
+  197,
+  239,
+  293,
+  353,
+  431,
+  521,
+  631,
+  761,
+  919,
+  1103,
+  1327,
+  1597,
+  1931,
+  2333,
+  2801,
+  3371,
+  4049,
+  4861,
+  5839,
+  7013,
+  8419,
+  10103,
+  12143,
+  14591,
+  17519,
+  21023,
+  25229,
+  30293,
+  36353,
+  43627,
+  52361,
+  62851,
+  75431,
+  90523,
+  108631,
+  130363,
+  156437,
+  187751,
+  225307,
+  270371,
+  324449,
+  389357,
+  467237,
+  560689,
+  672827,
+  807403,
+  968897,
+  1162687,
+  1395263,
+  1674319,
+  2009191,
+  2411033,
+  2893249,
+  3471899,
+  4166287,
+  4999559,
+  5999471,
+  7199369
+];
+
+const hashPrime = 101;
+
+const isPrime = (candidate: number): boolean => {
+  if ((candidate & 1) != 0) {
+    const limit = Math.ceil(Math.sqrt(candidate));
+    for (let divisor = 3; divisor <= limit; divisor += 2) {
+      if (candidate % divisor === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  return candidate === 2;
+};
+
+const getPrime = (min: number): number | undefined => {
+  if (min < 0) {
+    return undefined;
+  }
+
+  for (let i = 0, l = primes.length; i < l; i++) {
+    if (primes[i] >= min) {
+      return primes[i];
+    }
+  }
+
+  for (let i = min | 1; i < Number.MAX_SAFE_INTEGER; i += 2) {
+    if (isPrime(i) && (i - 1) % hashPrime !== 0) {
+      return i;
+    }
+  }
+
+  return min;
+};
+
+const getMinPrime = (): number => {
+  return primes[0];
+};
+
 const isObject = (item: unknown): boolean => {
   return item && typeof item === "object" && !Array.isArray(item);
 };
 
 const isDefined = (item: unknown): boolean => {
-  return item && typeof item !== "undefined";
+  return typeof item != typeof undefined;
 };
 
 const isFunction = (item: unknown): boolean => {
@@ -30,4 +145,38 @@ const defaultComparer = <T>(x: T, y: T): number => {
   return 1;
 };
 
-export { isObject, isDefined, isFunction, isString, defaultEqualityComparer, defaultComparer };
+/**
+ * Converts UTF-8 strings to {@link Uint8Array}
+ */
+const encoder: TextEncoder = new TextEncoder();
+
+const hashCode = (input: string): number => {
+  let hash = 2166136261;
+  const uint8array: Uint8Array = encoder.encode(input);
+
+  for (let i = 0, l = uint8array.length; i < l; i++) {
+    hash ^= uint8array[i];
+    hash += (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
+  }
+
+  return hash >>> 0;
+};
+
+const objectHasProperty = (
+  obj: Record<string, unknown>,
+  prop: string | number | symbol
+): boolean => Object.prototype.hasOwnProperty.call(obj, prop);
+
+export {
+  hashCode,
+  isObject,
+  isDefined,
+  isFunction,
+  isString,
+  defaultEqualityComparer,
+  defaultComparer,
+  isPrime,
+  getPrime,
+  getMinPrime,
+  objectHasProperty
+};
